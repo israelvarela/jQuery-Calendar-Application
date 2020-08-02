8e51d6229da00dc55e31ab217c7c293c91a6e4c6
Izzy V <israelreyvarela@gmail.com>
2020-08-01 21:24:50 -0500

    added business hours to the calendar

---
+++ index.js
@@ -0,0 +1,18 @@
+// Variables For Hours Of The Work Day //
+
+var currentDay = moment().format("dddd, MMMM Do");
+var workHours = [
+    "9am"
+    "10am"
+    "11am"
+    "12pm"
+    "1pm"
+    "2pm"
+    "3pm"
+    "4pm"
+    "5pm"
+    "6m"
+]
+
+var militaryHours =  [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+var currentHour = moment().hour();
